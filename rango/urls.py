afe9be9e2aff6752856e70765fa7e75e38898e31
Joshua Martino <2649949M@student.gla.ac.uk>
2023-01-23 13:10:33 +0000

    chapter 6

--- rango/urls.py
+++ rango/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('picture/', views.picture, name= "picture"),
     path('about/',views.about, name = "about" ),
+    path('category/<slug:category_name_slug>/', views.show_category, name = 'show_category'),
+    path('page/', views.show_page, name = 'show_page')
+
 ]
