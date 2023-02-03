4a23ef7a5acb2e1dbe22178c5a92e1e9b882de22
Joshua Martino <2649949M@student.gla.ac.uk>
2023-02-03 14:19:45 +0000

    added cat for chapter 4

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
@@ -27,4 +29,4 @@ urlpatterns = [
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
